b37584673b8bc80112f20faa5e9f50dec7f88475
Alex Incerti <alex.incerti@outlook.com>
2022-05-12 23:17:54 +0200

    titles

--- src/App.js
+++ src/App.js
@@ -17,6 +17,10 @@ function App() {
       <header className="App-header">
         <div className={'profile-container' + (transition?' slide-exit-active':'')} >
           <img src={"https://raw.githubusercontent.com/IncioMan/my-website/master/src/images/2022-01-23%2012.30.34.jpg"} className="App-logo" alt="logo" />
+          <div className='titles-container'>
+            <div>Software/Data Engineer</div>
+            <div>On-chain Data Analyst</div>
+          </div>
           <div className={'contacts-container'}>
             <a href={'https://twitter.com/IncioMan'}><BsTwitter size={'3em'} className='contact-logo'/></a>
             <a href={'https://github.com/IncioMan'}><BsGithub size={'3em'} className='contact-logo'/></a>
